608449cb1a25560f35f8aae256231a111460ea90
M.S <ms@hadronlang.com>
2026-02-16 17:18:41 +0100

    chore: add better error handling to file mapping

--- src/mapping/map.c
+++ src/mapping/map.c
@@ -6,7 +6,7 @@
 /*   By: mattcarniel <mattcarniel@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/06 15:45:50 by mattcarniel       #+#    #+#             */
-/*   Updated: 2026/02/09 18:17:02 by mattcarniel      ###   ########.fr       */
+/*   Updated: 2026/02/16 17:14:30 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,10 @@
 
 bool	is_valid_name(const char *path)
 {
-	const char *find;
+	const char	*find;
 
 	if (!path || strlen(path) < 5)
 		return (false);
-
 	find = memmem(path, strlen(path) + 1, ".cub\0", 5);
 	if (!find || find[-1] == '/')
 		return (false);
@@ -41,23 +40,22 @@ void	unmap_file(t_file *file)
 	*file = (t_file){0};
 }
 
-t_file	map_file(const char *path)
+t_file	file_open(const char *path)
 {
-	t_file	file;
-	struct stat st;
-	int	fd;
+	struct stat	st;
+	t_file		file;
+	int			fd;
 
-	if (!path)
-		return ((t_file){0}); //error
+	file.error = FERR_OK;
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
-		return ((t_file){0}); //error
+		return ((t_file){.error = FERR_OPEN});
 	fstat(fd, &st);
 	if (st.st_size <= 0)
-		return ((t_file){0}); //error
-	file.data = (char *)mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+		return ((t_file){.error = FERR_SIZE});
+	file.data = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	if (file.data == MAP_FAILED)
-		return ((t_file){0}); //error
+		return ((t_file){.error = FERR_MMAP});
 	close(fd);
 	file.path = path;
 	file.size = st.st_size;
--- src/mapping/mapping.h
+++ src/mapping/mapping.h
@@ -6,20 +6,28 @@
 /*   By: mattcarniel <mattcarniel@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 16:51:42 by mattcarniel       #+#    #+#             */
-/*   Updated: 2026/02/09 16:52:14 by mattcarniel      ###   ########.fr       */
+/*   Updated: 2026/02/16 17:15:24 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAPPING_H
 # define MAPPING_H
 
-#include <stddef.h>
+# include <stddef.h>
 
-typedef struct	s_file
+typedef enum e_file_error {
+	FERR_OK,
+	FERR_OPEN,
+	FERR_SIZE,
+	FERR_MMAP,
+}	t_file_error;
+
+typedef struct s_file
 {
-	const char	*path;
-	const char	*data;
-	size_t		size;
-}				t_file;
+	const char		*path;
+	const char		*data;
+	size_t			size;
+	t_file_error	error;
+}	t_file;
 
 #endif
